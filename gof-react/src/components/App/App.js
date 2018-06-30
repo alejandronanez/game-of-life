06abe0e6b348c650d1fd0ec84bd1a30c33cf723d
Alejandro Nanez <alejonanez@gmail.com>
2018-06-30 09:24:08 -0500

    completed game of life

--- gof-react/src/components/App/App.js
+++ gof-react/src/components/App/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Grid from 'components/Grid';
-import { createGrid } from 'helpers';
+import { createGrid, cloneArray } from 'helpers';
 
 export class App extends Component {
+  rows = 4;
+
   state = {
-    grid: createGrid(10, 10),
-    aliveCells: [[0, 0], [0, 1], [1, 0], [1, 3], [2, 1], [2, 2]],
+    generationCount: 0,
+    intervalId: 0,
+    grid: createGrid(this.rows, this.rows),
+    aliveCells: [[0, 0], [0, 1], [1, 0], [1, 3], [2, 1], [2, 2], [3, 1]],
   };
 
   componentDidMount() {
@@ -26,10 +30,80 @@ export class App extends Component {
     this.setState({ grid });
   };
 
+  getNeighbors = (x, y) => {
+    return [
+      [x - 1, y - 1],
+      [x - 1, y],
+      [x - 1, y + 1],
+      [x, y - 1],
+      [x, y + 1],
+      [x + 1, y - 1],
+      [x + 1, y],
+      [x + 1, y + 1],
+    ];
+  };
+
+  validateNeighbors = neighbors => {
+    return neighbors.filter(coord => {
+      return !(coord.includes(-1) || coord.includes(this.rows));
+    });
+  };
+
+  countAliveCells = neighbors => {
+    return neighbors.filter(([x, y]) => {
+      return this.state.grid[x][y];
+    }).length;
+  };
+
+  shouldToggleCell = (totalAliveNeighbors, x, y) => {
+    const cell = this.state.grid[x][y];
+
+    if (cell && (totalAliveNeighbors < 2 || totalAliveNeighbors > 3)) {
+      return true;
+    }
+
+    if (!cell && totalAliveNeighbors === 3) {
+      return true;
+    }
+  };
+
+  scanGrid = () => {
+    const newGrid = cloneArray(this.state.grid);
+
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.rows; j++) {
+        const neighbors = this.getNeighbors(i, j);
+        const validNeighbors = this.validateNeighbors(neighbors);
+        const totalAliveNeighbors = this.countAliveCells(validNeighbors);
+        if (this.shouldToggleCell(totalAliveNeighbors, i, j)) {
+          newGrid[i][j] = !newGrid[i][j];
+        }
+      }
+    }
+
+    this.setState(prevState => ({
+      generationCount: prevState.generationCount + 1,
+      grid: newGrid,
+    }));
+  };
+
+  startGame = () => {
+    const intervalId = setInterval(() => this.scanGrid(), 1000);
+
+    this.setState({ intervalId });
+  };
+
+  pauseGame = () => {
+    clearInterval(this.state.intervalId);
+  };
+
   render() {
     return (
       <div className="wrapper">
+        <h1>{this.state.generationCount} Generations</h1>
         <Grid grid={this.state.grid} />
+        <button onClick={this.startGame}>Play</button>
+        <button onClick={this.pauseGame}>Pause</button>
       </div>
     );
   }
--- gof-react/src/helpers/index.js
+++ gof-react/src/helpers/index.js
@@ -5,3 +5,7 @@ export function rowCreator(rowLength) {
 export function createGrid(rows = 50, cols = 50) {
   return Array.from({ length: rows }).map(() => rowCreator(cols));
 }
+
+export function cloneArray(arr) {
+  return JSON.parse(JSON.stringify(arr));
+}
